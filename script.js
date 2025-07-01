510d27b5218aa6d0895cdced2aa0a4992cbfc815
Ibra <xamxoev221@mail.ru>
2025-07-01 11:20:25 +0300

    landing-page

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const start_btn = document.getElementById("start_btn");
 
 start_btn.addEventListener("click", () => {
- window.location.href = "./login-page/index.html";
+ window.location.href = "./Login-page/index.html";
 })
 
  
